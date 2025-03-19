0327020cc1de9531be64608b73fd1b4a829f6859
Alejandro Marin <almarinca@unal.edu.co>
2025-03-19 12:01:30 -0500

    use Context in component TodoCounter

--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -1,10 +1,16 @@
 import './TodoCounter.css'
+import { useContext } from 'react'
+import { TodoContext } from '../../TodoContext'
 
-function TodoCounter({total, completed}) {
+function TodoCounter() {
+    const {
+        completedTodos,
+        totalTodos,
+    } = useContext(TodoContext)
     const completedMsg = "Congratulations! You have completed all your tasks"
     return (
         <h1 className='todo-heading'>
-            {(completed === total) ? completedMsg : `You have completed ${completed} of ${total} tasks`}
+            {(completedTodos === totalTodos) ? completedMsg : `You have completed ${completedTodos} of ${totalTodos} tasks`}
         </h1>
     )
 }
